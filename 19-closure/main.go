118092d9c85522a789ef1cb4888e2cc450f2a82c
Sofikul <sofikul.projects@gmail.com>
2022-06-17 16:00:05 +0530

    Update main.go

--- 19-closure/main.go
+++ 19-closure/main.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+// A closure is a function that references variables outside its own function body (scope).
+// In other words, a closure is an inner function that has access to the variables in the scope in which it was created.
+// This applies even when the outer function finishes execution and the scope gets destroyed.
+
 // example of closure function like javascript
 func nextSeqGen() func() int {
 	i := 0
