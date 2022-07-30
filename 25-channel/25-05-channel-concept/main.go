2751d04399487f7641e4c83a031ee8a3819c2c9e
Sofikul <sofikul.projects@gmail.com>
2022-07-30 08:24:45 +0530

    Update main.go

--- 25-channel/25-05-channel-concept/main.go
+++ 25-channel/25-05-channel-concept/main.go
@@ -7,20 +7,8 @@ func main() {
 
 	go testFn(ch)
 	fmt.Println(<-ch) // blocking
-
-	// for i := 0; i < 10; i++ {
-	// 	go testFn(ch, i)
-	// }
-
-	// for i := 0; i < 10; i++ {
-	// 	fmt.Println(<-ch)
-	// }
 }
 
 func testFn(ch chan int) {
 	ch <- 10
 }
-
-// func testFn(ch chan int, i int) {
-// 	ch <- i
-// }
